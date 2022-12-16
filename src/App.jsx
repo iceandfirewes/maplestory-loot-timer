be0fb4f8a3c841de625f9c099ff2cd480244b498
iceandfirewes <iceandfire29072000@gmail.com>
2022-12-16 22:40:59 +0700

    add ringTone after X time has elapsed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState,useEffect } from 'react'
+import { useState,useEffect, createRef } from 'react'
 import './App.css'
+import ringTone from './assets/vendarroRing.wav'
 function App() {
   const [active, setActive] = useState(false)
   const [timeMark, setTimeMark] = useState(Date.now())
   const [time, setTime] = useState(Date.now())
   const SECOND = 1000;
   const MINUTE = SECOND * 60;
-  const lootInterval = 120000;
-  const testlootInterval = 7000;
-  const [mobbingTime, setMobbingTime] = useState(100)
+  const lootInterval = 120000; //2minutes
+  const testlootInterval = 7000; //7s
+  const [mobbingTime, setMobbingTime] = useState(100000) //100s
+  const [boolTonePlayed, setBoolTonePlayed] = useState(false)
   function startTimer()
   {
     setActive(true)
@@ -17,8 +19,6 @@ function App() {
   function handleInput(event)
   {
     
-    setMobbingTime(event.target.value)
-    console.log(mobbingTime)
   }
   useEffect(() => {
     if(active)
@@ -29,9 +29,18 @@ function App() {
         //if(time - timeMark > testlootInterval)
         {
           setTimeMark(Date.now())
+          setBoolTonePlayed(false)
+        }
+        if(time - timeMark > mobbingTime)
+        {
+          if(!boolTonePlayed)
+          {
+            document.getElementById("ringTonePlayer").play()
+            setBoolTonePlayed(true)
+          }
         }
         setTime(Date.now())
-      }, 1000)
+      }, .5 * SECOND)
       return () => clearInterval(interval);
     }
   /**for some reason, setting this to active and time make the timer
@@ -68,8 +77,16 @@ function App() {
       <h4>Meso bag expire every 2 minutes. Thus, you would want to loot everything before that.</h4>
       <h4>Please set your mobbing time. for example, 1m40s.</h4>
       <h4>when that time has elapese, a ring tone will sound, you should loot at this time.</h4>
-      <input onChange={(event) => handleInput(event)}value={mobbingTime} placeholder="Seconds"></input>
+      <div>
+        <input className="secondInput" onChange={(event) => setMobbingTime(event.target.value*SECOND)}value={mobbingTime/1000} placeholder="Seconds"></input><span>s</span>
+      </div>
       <div>{timeDisplay}</div>
+      <audio 
+        style={{display:"none"}}
+        id="ringTonePlayer"
+        controls
+        src={ringTone}>
+      </audio>
       <button onClick={startTimer}>{active? "Reset Timer" : "Start Timer"}</button>
     </div>
   )
